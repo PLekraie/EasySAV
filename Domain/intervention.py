c1a64fdba31ec4d32510b070389fbca950bf7852
PLekraie <pauline.lekraie@gmail.com>
2021-03-01 11:17:15 +0100

    feat(model): add model for intervention and technicien

---
+++ Domain/intervention.py
@@ -0,0 +1,9 @@
+class Intervention:
+    def __init__(self, code, modalite, id_client):
+        self.code = code
+        self.modalite = modalite
+        self.etat = "non attribuée"
+        self.duree = 0
+        self.id_technicien = None
+        self.id_client = id_client
+
---
+++ Domain/technicien.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from Domain.intervention import Intervention
+
+
+class Technicien:
+    def __init__(self, id, nom, prenom ):
+        self.id = id
+        self.nom = nom
+        self.prenom = prenom
+        self.interventions: List[Intervention] = list()
